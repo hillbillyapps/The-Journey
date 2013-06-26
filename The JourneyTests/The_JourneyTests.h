6b1f212056eb6806906c1f03522cf1ed0cfad013
hillbillyapps <zackfletch00@gmail.com>
2013-06-25 19:12:01 -0500

    Tuesday, 7:00, June 25

---
+++ The JourneyTests/The_JourneyTests.h
@@ -0,0 +1,13 @@
+//
+//  The_JourneyTests.h
+//  The JourneyTests
+//
+//  Created by Zack Fletcher on 6/11/13.
+//  Copyright (c) 2013 The Journey. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface The_JourneyTests : SenTestCase
+
+@end
---
+++ The JourneyTests/The_JourneyTests.m
@@ -0,0 +1,32 @@
+//
+//  The_JourneyTests.m
+//  The JourneyTests
+//
+//  Created by Zack Fletcher on 6/11/13.
+//  Copyright (c) 2013 The Journey. All rights reserved.
+//
+
+#import "The_JourneyTests.h"
+
+@implementation The_JourneyTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in The JourneyTests");
+}
+
+@end
